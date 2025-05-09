69515f1828850cff439bf6da4d341c1b52bb856c
KihoLee <kiho2734@naver.com>
2025-05-09 21:35:08 +0900

    Add Hero file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import TopAdbar from "./components/TopAdBar";
 import Navbar from "./pages/Navbar";
+import Hero from "./pages/Hero";
 
 function App() {
   return (
     <div>
       <TopAdbar></TopAdbar>
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   );
 }
