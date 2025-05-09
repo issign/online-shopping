4e01957fbc1a14fb7561af07b16f9edb2d353f79
KihoLee <kiho2734@naver.com>
2025-05-09 20:39:49 +0900

    Modify TopAdbar style

--- src/components/TopAdbar.js
+++ src/components/TopAdbar.js
@@ -1,5 +1,9 @@
 function TopAdbar() {
-    return <div>Top</div>
+  return (
+    <div className="w-full flex justify-center items-center h-[40px] bg-black text-white">
+      <span className="text-sm">신규회원가입하고 4천원 할인받자.</span>
+    </div>
+  );
 }
 
 export default TopAdbar;
