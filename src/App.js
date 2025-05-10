dc29d007e17e9d7f8ba38977dddba1d52dc1bc39
KihoLee <kiho2734@naver.com>
2025-05-10 11:00:59 +0900

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import TopAdbar from "./components/TopAdBar";
 import Navbar from "./pages/Navbar";
 import Hero from "./pages/Hero";
+import axios from "axios";
 
 function App() {
+  const create = async () => {
+    const res = await axios.get("http://localhost:3001/products");
+
+    console.log(res);
+  };
+
   return (
     <div>
       <div className="w-screen h-screen">
         <TopAdbar></TopAdbar>
         <Navbar></Navbar>
         <Hero></Hero>
+        <div onClick={create}>Click</div>
       </div>
     </div>
   );
